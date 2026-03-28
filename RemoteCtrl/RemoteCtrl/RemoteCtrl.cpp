f65c92a262f7fb4f6babb221785726fc9d1a4fc2
xql <2969699014@qq.com>
2026-03-28 10:39:51 +0800

    屏幕截图和发送

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -6,6 +6,7 @@
 #include "RemoteCtrl.h"
 #include "ServerSocket.h"
 #include <direct.h>
+#include <atlimage.h>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -58,7 +59,8 @@ typedef struct file_info {
     char szFileName[256];//文件名
 }FILEINFO, * PFILEINFO;
 
-int MakeDiretoryInfo() {
+int MakeDiretoryInfo() 
+{
     std::string strPath;
     //std::list<FILEINFO> lstFileInfos;
     if (CServerSocket::GetInstance()->GetFilePath(strPath) == false) {
@@ -100,7 +102,8 @@ int MakeDiretoryInfo() {
 }
 
 
-int RunFile() {
+int RunFile() 
+{
 
     std::string strPath;
     CServerSocket::GetInstance()->GetFilePath(strPath);
@@ -110,7 +113,8 @@ int RunFile() {
     return 0;
 }
 
-int DownloadFile() {
+int DownloadFile() 
+{
     std::string strPath;
     CServerSocket::GetInstance()->GetFilePath(strPath);
 	long long data = 0;
@@ -140,7 +144,8 @@ int DownloadFile() {
 	return 0;
 }
 
-int MouseEvent() {
+int MouseEvent() 
+{
 	MOUSEEV mouse;
     if(CServerSocket::GetInstance()->GetMouseEvent(mouse)) {
 		DWORD nFlags = 0;
@@ -230,6 +235,39 @@ int MouseEvent() {
     return 0;
 }
 
+int SendScreen() 
+{
+    CImage screen;//GDI
+    HDC hScreen = ::GetDC(NULL);
+	int nBitPerPixel = GetDeviceCaps(hScreen, BITSPIXEL);//24
+    int nWidth = GetDeviceCaps(hScreen, HORZRES);
+    int nHeight = GetDeviceCaps(hScreen, VERTRES);
+    screen.Create(nWidth, nHeight, nBitPerPixel);
+    BitBlt(screen.GetDC(), 0, 0, 1920, 1020, hScreen, 0, 0, SRCCOPY);
+    ReleaseDC(NULL, hScreen);
+    HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, 0);
+    if (hMem == NULL) return -1;
+    IStream* pStream = NULL;    
+    HRESULT ret = CreateStreamOnHGlobal(hMem, TRUE, &pStream);
+    if (ret == S_OK) {
+        screen.Save(pStream, Gdiplus::ImageFormatPNG);
+        LARGE_INTEGER bg = { 0 };
+        pStream->Seek(bg,STREAM_SEEK_SET,NULL);
+        PBYTE pData = (PBYTE)GlobalLock(hMem);
+        SIZE_T nSize = GlobalSize(hMem);
+        CPacket pack(6, pData, 0);
+        CServerSocket::GetInstance()->Send(pack);
+        GlobalUnlock(hMem);
+    }
+
+    pStream->Release();
+    GlobalFree(hMem);
+
+    screen.ReleaseDC();
+	return 0;
+}
+
+
 int main()
 {
     int nRetCode = 0;
@@ -285,6 +323,9 @@ int main()
             case 5://鼠标控制
                 MouseEvent();
 				break;
+            case 6://发送屏幕内容==》发送屏幕的截图
+                SendScreen();
+                break;
             }
            
 
